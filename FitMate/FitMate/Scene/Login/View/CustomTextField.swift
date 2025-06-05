e09a5c4a37062f22189f62994bc03cce2d76fb03
Sophie4han <hanshirley775@gmail.com>
2025-06-05 23:23:53 +0900

    [Design]#4-LoginViewController UI 작업 1차

---
+++ FitMate/FitMate/Scene/Login/View/CustomTextField.swift
@@ -0,0 +1,68 @@
+//
+//  CustomTextField.swift
+//  FitMate
+//
+//  Created by Sophie on 6/5/25.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+class CustomTextField: UITextField {
+    
+    private let eyeToggleButton = UIButton()
+    private let disposeBag = DisposeBag()
+    private let isSecureRelay = BehaviorRelay<Bool>(value: true)
+    
+    init(placeholder: String, isSecure: Bool = true) {
+        super.init(frame: .zero)
+        self.placeholder = placeholder
+        self.attributedPlaceholder = NSAttributedString(
+            string: placeholder,
+            attributes: [.foregroundColor: UIColor.lightGray] // 컬러 변경 필요
+        )
+        self.isSecureTextEntry = isSecure //
+        self.borderStyle = .line
+        self.layer.borderColor = UIColor.systemPurple.cgColor // 컬러 변경 필요
+        self.layer.borderWidth = 1.5
+        self.backgroundColor = .darkGray // 컬러 변경 필요
+        self.translatesAutoresizingMaskIntoConstraints = false
+        
+        setEyeToggle()
+        bind()
+        
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setEyeToggle() {
+        eyeToggleButton.setImage(UIImage(systemName: "eye.slash"), for: .normal)
+        eyeToggleButton.tintColor = .lightGray
+        
+        // 우측 배치
+        rightView = eyeToggleButton
+        rightViewMode = .always
+    }
+    
+    func bind() {
+        eyeToggleButton.rx.tap
+            .withLatestFrom(isSecureRelay) // 옵저버블의 최신값 가져옴
+            .map { !$0 } // 값 변형
+            .bind(to: isSecureRelay) // bind는 종결 연산자라 마지막에 써야 함
+        
+        isSecureRelay
+            .asDriver(onErrorJustReturn: true)
+            .drive(onNext:{ [weak self] isSecure in
+                self?.isSecureTextEntry = isSecure
+                
+                let icon = isSecure ? "eye.slash" : "eye"
+                self?.eyeToggleButton.setImage(UIImage(systemName: icon), for: .normal)
+            })
+            .disposed(by: disposeBag)
+        
+    }
+
+}
