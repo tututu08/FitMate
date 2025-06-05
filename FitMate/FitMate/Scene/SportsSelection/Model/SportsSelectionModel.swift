e12d410ea3d38b0ab449b3136aeae829b2f74721
eunseo <neruneru33@naver.com>
2025-06-05 17:25:19 +0900

    [Design] #1 - CarouselCell 생성중

--- FitMate/FitMate/Scene/SportsSelection/Model/SportsSelectionModel.swift
+++ FitMate/FitMate/Scene/SportsSelection/Model/SportsSelectionModel.swift
@@ -4,3 +4,62 @@
 //  Created by 강성훈 on 6/5/25.
 //
 
+import UIKit
+
+class CarouselCell: UICollectionViewCell {
+    private let imageBackgroundView: UIView = {
+        let backgroundView = UIView()
+        backgroundView.clipsToBounds = true
+        backgroundView.layer.cornerRadius = 20
+        backgroundView.backgroundColor = .systemGray6 // 변경해야함
+        return backgroundView
+    }()
+    private var imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        return imageView
+    }()
+    private var titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 17, weight: .bold)
+        label.textColor = .black
+        return label
+    }()
+    private let calorieLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 13, weight: .regular)
+        label.textColor = .black
+        return label
+    }()
+    private var calorieTextLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 13, weight: .regular)
+        label.textColor = .black
+        return label
+    }()
+    private let exerciseDescriptionLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 13, weight: .regular)
+        label.textColor = .black
+        return label
+    }()
+    private var exerciseDescriptionTextLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 13, weight: .regular)
+        label.textColor = .gray
+        return label
+    }()
+    private let exerciseEffectLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 13, weight: .regular)
+        label.textColor = .black
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
+}
--- FitMate/FitMate/Scene/SportsSelection/View/SportsSelectionViewController.swift
+++ FitMate/FitMate/Scene/SportsSelection/View/SportsSelectionViewController.swift
@@ -4,3 +4,8 @@
 //  Created by 강성훈 on 6/5/25.
 //
 
+import UIKit
+
+class SSportsSelectionViewController: UIViewController {
+    
+}
