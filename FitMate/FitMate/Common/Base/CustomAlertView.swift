6b1213e50730ad626a3d555efecdc468c9c908fa
Sophie4han <hanshirley775@gmail.com>
2025-06-08 14:35:16 +0900

    [Design] CustomAlertVIew 구현 완료 및 텍스트 필드 eyeToggleButton 삭제

---
+++ FitMate/FitMate/Common/Base/CustomAlertView.swift
@@ -0,0 +1,20 @@
+//
+//  CustomAlertView.swift
+//  FitMate
+//
+//  Created by Sophie on 6/8/25.
+//
+
+import UIKit
+
+class CustomAlertView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
--- FitMate/FitMate/Scene/Login/View/CustomTextField.swift
+++ FitMate/FitMate/Scene/Login/View/CustomTextField.swift
@@ -15,21 +15,21 @@ class CustomTextField: UITextField {
     private let disposeBag = DisposeBag()
     private let isSecureRelay = BehaviorRelay<Bool>(value: true)
     
-    init(placeholder: String /*isSecure: Bool = true*/) {
+    init(placeholder: String) {
         super.init(frame: .zero)
         self.placeholder = placeholder
         self.attributedPlaceholder = NSAttributedString(
             string: placeholder,
             attributes: [.foregroundColor: UIColor.lightGray] // 컬러 변경 필요
         )
-//        self.isSecureTextEntry = isSecure
+
         self.borderStyle = .line
         self.layer.borderColor = UIColor.systemPurple.cgColor // 컬러 변경 필요
         self.layer.borderWidth = 1.5
         self.backgroundColor = .darkGray // 컬러 변경 필요
         self.translatesAutoresizingMaskIntoConstraints = false
         
-//        setEyeToggle()
+
         bind()
     }
     
@@ -43,20 +43,6 @@ class CustomTextField: UITextField {
     
     override func editingRect(forBounds bounds: CGRect) -> CGRect {
         return bounds.inset(by: UIEdgeInsets(top: 0, left: 12, bottom: 0, right: 0))
-    }
-    
-//    func setEyeToggle() {
-//        eyeToggleButton.setImage(UIImage(systemName: "eye.slash"), for: .normal)
-//        eyeToggleButton.tintColor = .lightGray
-//        
-//        // 우측 배치
-//        rightView = eyeToggleButton
-//        rightViewMode = .always
-//    }
-    
-    override func rightViewRect(forBounds bounds: CGRect) -> CGRect {
-        let original = super.rightViewRect(forBounds: bounds)
-        return original.offsetBy(dx: -8, dy: 0)
     }
     
     func bind() {
@@ -74,7 +60,6 @@ class CustomTextField: UITextField {
                 self?.eyeToggleButton.setImage(UIImage(systemName: icon), for: .normal)
             })
             .disposed(by: disposeBag)
-        
     }
     
 }
