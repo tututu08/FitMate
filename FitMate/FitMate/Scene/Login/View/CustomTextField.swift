b8b9bd789a82c9de7008b6d0f2e9386b68b36c87
Sophie4han <hanshirley775@gmail.com>
2025-06-10 17:34:17 +0900

    [Chore] 서비스 인포 제거

--- FitMate/FitMate/Scene/Login/View/CustomTextField.swift
+++ FitMate/FitMate/Scene/Login/View/CustomTextField.swift
@@ -15,21 +15,21 @@ class CustomTextField: UITextField {
     private let disposeBag = DisposeBag()
     private let isSecureRelay = BehaviorRelay<Bool>(value: true)
     
-    init(placeholder: String, isSecure: Bool = true) {
+    init(placeholder: String /*isSecure: Bool = true*/) {
         super.init(frame: .zero)
         self.placeholder = placeholder
         self.attributedPlaceholder = NSAttributedString(
             string: placeholder,
             attributes: [.foregroundColor: UIColor.lightGray] // 컬러 변경 필요
         )
-        self.isSecureTextEntry = isSecure //
+//        self.isSecureTextEntry = isSecure
         self.borderStyle = .line
         self.layer.borderColor = UIColor.systemPurple.cgColor // 컬러 변경 필요
         self.layer.borderWidth = 1.5
         self.backgroundColor = .darkGray // 컬러 변경 필요
         self.translatesAutoresizingMaskIntoConstraints = false
         
-        setEyeToggle()
+//        setEyeToggle()
         bind()
     }
     
@@ -45,14 +45,14 @@ class CustomTextField: UITextField {
         return bounds.inset(by: UIEdgeInsets(top: 0, left: 12, bottom: 0, right: 0))
     }
     
-    func setEyeToggle() {
-        eyeToggleButton.setImage(UIImage(systemName: "eye.slash"), for: .normal)
-        eyeToggleButton.tintColor = .lightGray
-        
-        // 우측 배치
-        rightView = eyeToggleButton
-        rightViewMode = .always
-    }
+//    func setEyeToggle() {
+//        eyeToggleButton.setImage(UIImage(systemName: "eye.slash"), for: .normal)
+//        eyeToggleButton.tintColor = .lightGray
+//        
+//        // 우측 배치
+//        rightView = eyeToggleButton
+//        rightViewMode = .always
+//    }
     
     override func rightViewRect(forBounds bounds: CGRect) -> CGRect {
         let original = super.rightViewRect(forBounds: bounds)
