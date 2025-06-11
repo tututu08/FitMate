71462851c0df5c4f1c8568e632e49f9f039bb972
eunseo <neruneru33@naver.com>
2025-06-11 12:41:52 +0900

    [Chore] 중복 파일 삭제

--- FitMate/FitMate/Common/Base/BaseViewController.swift
+++ FitMate/FitMate/Common/Base/BaseViewController.swift
@@ -28,10 +28,6 @@ class BaseViewController: UIViewController {
     func bindViewModel() {
         // RxSwift 등 바인딩 처리
     }
-    
-    func bindViewModel() {
-        // RxSwift 등 바인딩 처리
-    }
 
 }
 
