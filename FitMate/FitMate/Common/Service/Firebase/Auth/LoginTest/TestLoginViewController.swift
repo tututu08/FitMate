10886058701b67e8b0bf536e56fb98397617c0b0
NH <71053769+mnh4140@users.noreply.github.com>
2025-06-15 19:47:05 +0900

    [Merge] #55 Firebase Auth를 이용한 구글 로그인 기능 구현 (#57)
    * [Feat] #55 구글 로그인 라이브러리 추가
    
    * [Feat] #55 구글 로그인 연동 설정
    
    * [Feat] #55 구글 로그인 인증 메서드 추가
    
    * [Feat] #55 Firebase Auth를 통한 구글 로그인 기능 구현
    
    * [Feat] #55 MainView 에 구글 로그인 기능 연결

--- FitMate/FitMate/Common/Service/Firebase/Auth/LoginTest/TestLoginViewController.swift
+++ FitMate/FitMate/Common/Service/Firebase/Auth/LoginTest/TestLoginViewController.swift
@@ -1,42 +1,77 @@
 //
+//  LoginViewController.swift
 //  FitMate
 //
-//  Created by 강성훈 on 6/5/25.
+//  Created by NH on 6/12/25.
 //
 
 import UIKit
-import SnapKit
+import GoogleSignIn
 import RxSwift
-import RxCocoa
+import RxRelay
+import FirebaseAuth
 
-class LoginViewController: BaseViewController {
+class TestLoginViewController: UIViewController {
+
+    let titleLabel = UILabel() // 타이틀 라벨 / 환영합니다.
+    let googleLoginButton = UIButton() // 구글 로그인 버튼
+
+    let viewModel = TestLoginViewModel() // 뷰모델
+    var disposeBag = DisposeBag() // 디스포스백
     
-    let viewModel = LoginViewModel() // 뷰모델
     let googleLoginTrigger = PublishRelay<Void>() // 로그인 버튼 클릭 이벤트 전달
     let nextViewRelay = PublishRelay<Void>() // 다음 버튼 클릭 이벤트 전달
-    
-    let logInView = LoginView()
-    
-    override func loadView() {
-        super.loadView()
-        self.view = logInView
-    }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        setupUI()
+        bindViewModel()
         bindButton()
     }
+
+    private func setupUI() {
+        view.backgroundColor = .white
+
+        titleLabel.text = "환영합니다"
+        titleLabel.font = .boldSystemFont(ofSize: 28)
+        titleLabel.textAlignment = .center
+
+        googleLoginButton.setImage(.googleLogo, for: .normal)
+        googleLoginButton.imageView?.contentMode = .scaleAspectFit
+
+        view.addSubview(titleLabel)
+        view.addSubview(googleLoginButton)
+
+        titleLabel.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(100)
+        }
+
+        googleLoginButton.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(titleLabel.snp.bottom).offset(40)
+            
+        }
+        
+        googleLoginButton.imageView?.snp.makeConstraints { make in
+            make.width.height.equalTo(50)
+            
+        }
+    }
+
     /// 버튼 이벤트를 ViewModel로 전달
     private func bindButton() {
-        self.logInView.googleLogin.rx.tap
+        googleLoginButton.rx.tap
             // UI에서 발생한 이벤트를 Relay로 보내는 상황
             .bind(to: googleLoginTrigger) // relay에 이벤트(빈 값)를 흘려보내라
             .disposed(by: disposeBag)
     }
-    
-    override func bindViewModel() {
+
+    /// 뷰 모델 바인드
+    private func bindViewModel() {
         // 명시적인 흐름과 불변성(immutable)을 유지하기 위해서 이래와 같이 선언
-        let input = LoginViewModel.Input(
+        let input = TestLoginViewModel.Input(
             // ViewModel 에서 값을 방출하지 못하게 막기위해 asObservable 사용
             // asObservable : 뷰 모델에 구독만 가능하게 변환
             // 이렇게 되면 값을 방출 할 수 없음,
@@ -44,7 +79,7 @@ class LoginViewController: BaseViewController {
             //.onNext(), .accpet()로 값을 방출하는건 불가능
             googleLoginTrigger: googleLoginTrigger.asObservable()
         )
-        
+
         let output = viewModel.transform(input: input, presentingVC: self)
         
         output.navigation
