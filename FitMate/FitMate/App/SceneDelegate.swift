9820c731a850081e4c9d0723069ae75b2bab3121
NH <mnh4140@gmail.com>
2025-06-16 17:39:03 +0900

    [Chore] #58 앱 시작 화면 변경

--- FitMate/FitMate/App/SceneDelegate.swift
+++ FitMate/FitMate/App/SceneDelegate.swift
@@ -16,7 +16,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
 
         let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = TabBarController(uid: "JAZVy7wRXMUgI5aXH4GCuVG4Tyk2")
+        window.rootViewController = LoginViewController()
         window.makeKeyAndVisible()
         self.window = window
     }
