8f84a73eb28ca416d73305bd179e8c4c9eee9d2f
NH <mnh4140@gmail.com>
2025-06-18 01:24:58 +0900

    [Fix] #93 dev 오류 코드 수정

--- FitMate/FitMate/Scene/Login/ViewModel/LoginViewModel.swift
+++ FitMate/FitMate/Scene/Login/ViewModel/LoginViewModel.swift
@@ -134,24 +134,61 @@ final class LoginViewModel {
                     .catch { .just(.failure($0)) }
                     .asObservable()
             }
+//            .flatMapLatest { result -> Observable<LoginNavigation> in
+//                // Firebase 로그인 결과에 따른 다음 화면 결정
+//                switch result {
+//                case .success(let user as FirebaseAuth.User):
+//                    let uid = user.uid
+//                    // Firestore에 사용자 문서가 있는지 확인
+//                    return FirestoreService.shared
+//                        .fetchDocument(collectionName: "users", documentName: uid)
+//                        .map { _ in .goToMainViewController(uid: uid) } // 있으면 해당 화면으로 이동
+//                        .catch { _ in
+//                            // 문서가 없으면 새로 생성 후 이동
+//                            FirestoreService.shared
+//                                .createUserDocument(uid: uid)
+//                                .map { .goToMainViewController(uid: uid) }
+//                        }
+//                        .asObservable()
+//                case .failure(let error):
+//                    return .just(.error(error.localizedDescription))
+//                }
+//            }
+        // MARK: - 수정 필요
             .flatMapLatest { result -> Observable<LoginNavigation> in
-                // Firebase 로그인 결과에 따른 다음 화면 결정
+                // 로그인 결과에 따라 Firestore 조회 및 화면 분기
                 switch result {
-                case .success(let user as FirebaseAuth.User):
-                    let uid = user.uid
-                    // Firestore에 사용자 문서가 있는지 확인
+                case .success(let user): // 성공 시, 로그인 사용자 정보를 가져옴
+                    let uid = user.uid // uid 정보 저장
+                    // Firestore 조회를 Observable로 감쌈
                     return FirestoreService.shared
-                        .fetchDocument(collectionName: "users", documentName: uid)
-                        .map { _ in .goToMainViewController(uid: uid) } // 있으면 해당 화면으로 이동
-                        .catch { _ in
-                            // 문서가 없으면 새로 생성 후 이동
-                            FirestoreService.shared
-                                .createUserDocument(uid: uid)
-                                .map { .goToMainViewController(uid: uid) }
+                        .fetchDocument(collectionName: "users", documentName: uid) // 사용자 문서 검색
+                        .flatMap { data -> Single<LoginNavigation> in
+                            // 닉네임이 있고
+                            if let nickname = data["nickname"] as? String,
+                               !nickname.isEmpty {
+                                // 메이트가 있으면
+                                if let mate = data["mate"] as? String, !mate.isEmpty {
+                                    // 메인 뷰로 이동
+                                    return .just(.goToMainViewController(uid: uid))
+                                } else {
+                                    // 메이트가 없으면 메이트 등록 화면으로 이동
+                                    return .just(.goToInputMateCode(uid: uid))
+                                }
+                            } else {
+                                // 닉네임이 없으면 닉네임 입력 화면으로 이동
+                                return .just(.goToInputNickName(uid: uid))
+                            }
+                        }
+                        .catch { _ in // 사용자 문서가 존재하지 않다면
+                            return FirestoreService.shared
+                                .createUserDocument(uid: uid) // 사용자 uid 로 문서를 생성
+                                .map { .goToInputNickName(uid: uid) } // .just(.goToInputNickName(uid: uid)로 반환
                         }
                         .asObservable()
-                case .failure(let error):
-                    return .just(.error(error.localizedDescription))
+                    
+                case .failure(let error): // 로그인 정보를 가져오지 못하면
+                    return .just(.error(error.localizedDescription)) // 에러 메시지 출력
                 }
             }
         
