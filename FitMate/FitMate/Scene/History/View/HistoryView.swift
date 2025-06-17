73b9755c5132d0577969a671764c7718d5e45639
HyoungYoon <mkl0285@naver.com>
2025-06-18 03:33:50 +0900

    [refactor] #95 기록페이지 UI 구조 변경

--- FitMate/FitMate/Scene/History/View/HistoryView.swift
+++ FitMate/FitMate/Scene/History/View/HistoryView.swift
@@ -1,12 +1,3 @@
-//
-//  FitMate
-//
-//  Created by 강성훈 on 6/5/25.
-//
-// HistoryView.swift
-//
-//
-
 import UIKit
 import SnapKit
 
@@ -19,7 +10,7 @@ final class HistoryView: UIView {
         layout.scrollDirection = .horizontal
         layout.minimumInteritemSpacing = 0
         layout.minimumLineSpacing = 0
-        layout.sectionInset = .zero 
+        layout.sectionInset = .zero
         let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
         collectionView.showsHorizontalScrollIndicator = false
         collectionView.backgroundColor = .clear
@@ -43,19 +34,16 @@ final class HistoryView: UIView {
         return label
     }()
 
-    let backButton: UIButton = { 
-        let button = UIButton(type: .system)
-        button.setImage(UIImage(systemName: "chevron.left"), for: .normal)
-        button.tintColor = .white
-        return button
-    }()
-
     override init(frame: CGRect) {
         super.init(frame: frame)
-        backgroundColor = UIColor(red: 10/255, green: 10/255, blue: 10/255, alpha: 1.0)
+        backgroundColor = UIColor(named: "Background900")
 
         categoryCollectionView.register(CategoryCell.self, forCellWithReuseIdentifier: CategoryCell.identifier)
-        recordCollectionView.register(RecordCell.self, forCellWithReuseIdentifier: RecordCell.identifier)
+        recordCollectionView.register(WalkRecordCell.self, forCellWithReuseIdentifier: WalkRecordCell.identifier)
+        recordCollectionView.register(JumpRopeRecordCell.self, forCellWithReuseIdentifier: JumpRopeRecordCell.identifier)
+        recordCollectionView.register(BicycleRecordCell.self, forCellWithReuseIdentifier: BicycleRecordCell.identifier)
+        recordCollectionView.register(RunRecordCell.self, forCellWithReuseIdentifier: RunRecordCell.identifier)
+        recordCollectionView.register(PlankRecordCell.self, forCellWithReuseIdentifier: PlankRecordCell.identifier)
 
         setupLayout()
     }
@@ -65,30 +53,23 @@ final class HistoryView: UIView {
     }
 
     private func setupLayout() {
-        addSubview(backButton)
         addSubview(titleLabel)
         addSubview(categoryCollectionView)
         addSubview(categoryUnderlineView)
         addSubview(recordCollectionView)
 
-        backButton.snp.makeConstraints {
-            $0.top.equalTo(safeAreaLayoutGuide.snp.top).offset(12)
-            $0.leading.equalToSuperview().offset(16)
-            $0.width.height.equalTo(24)
-        }
-
         titleLabel.snp.makeConstraints {
-            $0.centerY.equalTo(backButton.snp.centerY)
+            $0.top.equalTo(safeAreaLayoutGuide.snp.top).offset(12)
             $0.centerX.equalToSuperview()
         }
 
         categoryCollectionView.snp.makeConstraints {
-            $0.top.equalTo(backButton.snp.bottom).offset(16)
+            $0.top.equalTo(titleLabel.snp.bottom).offset(16)
             $0.leading.trailing.equalToSuperview()
             $0.height.equalTo(40)
         }
 
-        categoryUnderlineView.backgroundColor = UIColor(red: 138/255, green: 43/255, blue: 226/255, alpha: 1.0)
+        categoryUnderlineView.backgroundColor = UIColor(named: "Primary500")
         categoryUnderlineView.snp.makeConstraints {
             $0.top.equalTo(categoryCollectionView.snp.bottom)
             $0.leading.trailing.equalToSuperview()
--- FitMate/FitMate/Scene/History/View/HistoryViewController.swift
+++ FitMate/FitMate/Scene/History/View/HistoryViewController.swift
@@ -1,11 +1,3 @@
-//
-//  FitMate
-//
-//  Created by 강성훈 on 6/5/25.
-//
-//
-//
-
 import UIKit
 import RxSwift
 import RxCocoa
@@ -15,31 +7,38 @@ final class HistoryViewController: UIViewController, UICollectionViewDelegateFlo
     private let rootView = HistoryView()
     private let viewModel = HistoryViewModel()
     private let disposeBag = DisposeBag()
-    
-    // 로그인 유저의 uid
+
+    private let selectedCategorySubject = PublishSubject<ExerciseType>()
+
     private let uid: String
-    
-    // 초기화 함수
+
     init(uid: String) {
-        self.uid = uid // 의존성 주입
+        self.uid = uid
         super.init(nibName: nil, bundle: nil)
     }
-    
+
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     override func loadView() {
         self.view = rootView
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: false)
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         rootView.recordCollectionView.delegate = self
+        rootView.recordCollectionView.dataSource = self
         rootView.categoryCollectionView.delegate = self
+
         viewModel.loadMockData()
         bindViewModel()
-        
+
         let initialIndexPath = IndexPath(item: 0, section: 0)
         rootView.categoryCollectionView.selectItem(at: initialIndexPath, animated: false, scrollPosition: [])
     }
@@ -49,34 +48,75 @@ final class HistoryViewController: UIViewController, UICollectionViewDelegateFlo
             .bind(to: rootView.categoryCollectionView.rx.items(
                 cellIdentifier: CategoryCell.identifier,
                 cellType: CategoryCell.self)
-            ) { index, type, cell in
+            ) { [weak self] index, type, cell in
                 cell.configure(with: type.rawValue)
-                cell.isSelected = (type == self.viewModel.selectedCategory.value)
+                let currentSelected = try? self?.viewModel.currentFilteredRecords.first?.type
+                cell.isSelected = (type == currentSelected)
             }
             .disposed(by: disposeBag)
 
         rootView.categoryCollectionView.rx.itemSelected
             .map { ExerciseType.allCases[$0.item] }
-            .bind(to: viewModel.selectedCategory)
+            .bind(to: selectedCategorySubject)
             .disposed(by: disposeBag)
 
-        viewModel.filteredRecords
-            .drive(rootView.recordCollectionView.rx.items(
-                cellIdentifier: RecordCell.identifier,
-                cellType: RecordCell.self)
-            ) { index, record, cell in
-                cell.configure(with: record)
-            }
+        let input = HistoryViewModel.Input(selectedCategory: selectedCategorySubject.asObservable())
+        let output = viewModel.transform(input: input)
+
+        output.filteredRecords
+            .drive(onNext: { [weak self] _ in
+                self?.rootView.recordCollectionView.reloadData()
+            })
             .disposed(by: disposeBag)
     }
 
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         if collectionView == rootView.recordCollectionView {
             let width = collectionView.frame.width - 32
-            return CGSize(width: width, height: 100)
+            return CGSize(width: width, height: 120)
         } else {
             let width: CGFloat = floor(collectionView.frame.width / CGFloat(ExerciseType.allCases.count))
             return CGSize(width: width, height: 40)
+        }
+    }
+}
+
+extension HistoryViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return viewModel.currentFilteredRecords.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let record = viewModel.currentFilteredRecords[indexPath.item]
+
+        switch record.type {
+        case .walk:
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: WalkRecordCell.identifier, for: indexPath) as! WalkRecordCell
+            cell.configure(with: record)
+            return cell
+
+        case .jumprope:
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: JumpRopeRecordCell.identifier, for: indexPath) as! JumpRopeRecordCell
+            cell.configure(with: record)
+            return cell
+
+        case .bicycle:
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: BicycleRecordCell.identifier, for: indexPath) as! BicycleRecordCell
+            cell.configure(with: record)
+            return cell
+            
+        case .run:
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: RunRecordCell.identifier, for: indexPath) as! RunRecordCell
+            cell.configure(with: record)
+            return cell
+        
+        case .plank:
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PlankRecordCell.identifier, for: indexPath) as! PlankRecordCell
+            cell.configure(with: record)
+            return cell
+            
+        default:
+            fatalError("종목 없음")
         }
     }
 }
