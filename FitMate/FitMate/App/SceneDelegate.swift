b9f269cd397ce59140590ba89259f7e182538679
eunseo <neruneru33@naver.com>
2025-06-17 20:37:16 +0900

    [Fix] #75 목표치 저장 단위 나누기

--- FitMate/FitMate/App/SceneDelegate.swift
+++ FitMate/FitMate/App/SceneDelegate.swift
@@ -16,7 +16,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
 
         let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = AppleLoginTestViewController()
+        window.rootViewController = LoginViewController()
         window.makeKeyAndVisible()
         self.window = window
     }
