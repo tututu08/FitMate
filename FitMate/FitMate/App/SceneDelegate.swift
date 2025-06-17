0263c060d1bbb081352b3094993984c4f26c54d2
HyoungYoon <mkl0285@naver.com>
2025-06-18 04:38:49 +0900

    [Chore] #95 마이페이지 UI 변경사항 수정

--- FitMate/FitMate/App/SceneDelegate.swift
+++ FitMate/FitMate/App/SceneDelegate.swift
@@ -16,7 +16,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
 
         let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = UINavigationController(rootViewController: HistoryViewController(uid: "test")) 
+        window.rootViewController = UINavigationController(rootViewController: MypageViewController(uid: "test")) 
         window.makeKeyAndVisible()
         self.window = window
     }
--- FitMate/FitMate/Scene/History/ViewModel/HistoryViewModel.swift
+++ FitMate/FitMate/Scene/History/ViewModel/HistoryViewModel.swift
@@ -1,3 +1,4 @@
+
 import RxSwift
 import RxCocoa
 
