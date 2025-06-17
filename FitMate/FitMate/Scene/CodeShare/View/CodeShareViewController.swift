1f02ffe37274b22ce5304dc6cb76c0a06f1eb35a
NH <mnh4140@gmail.com>
2025-06-18 00:46:20 +0900

    [Feat] #89 CodeShareViewController 구현

--- FitMate/FitMate/Scene/CodeShare/View/CodeShareViewController.swift
+++ FitMate/FitMate/Scene/CodeShare/View/CodeShareViewController.swift
@@ -8,70 +8,204 @@ import UIKit
 import RxSwift
 import RxCocoa
 
-class CodeShareViewController: BaseViewController {
-    
+final class CodeShareViewController: BaseViewController {
+
     private let codeShareView = CodeShareView()
     private let viewModel: CodeShareViewModel
-    
-    private let uid: String // 로그인 사용자 uid
-    
+    private let uid: String
+
     init(uid: String) {
-        self.uid = uid // 의존성 주입
-        print("uid : \(uid)")
-        viewModel = CodeShareViewModel(uid: uid)
+        self.uid = uid
+        self.viewModel = CodeShareViewModel(uid: uid)
         super.init(nibName: nil, bundle: nil)
     }
-    
+
     @MainActor required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     override func loadView() {
         self.view = codeShareView
     }
-    
-    // 네이게이션 숨기기
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        navigationController?.setNavigationBarHidden(true, animated: false)
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        bind()
     }
-    
-    override func bindViewModel() {
-        // 메이트 코드 입력 버튼
-        codeShareView.mateCodeButton.rx.tap
-            .asDriver(onErrorDriveWith: .empty())
-            .drive(onNext: { [weak self] in
-                let next = MateCodeViewController()
-                self?.navigationController?.pushViewController(next, animated: true)
-            })
-            .disposed(by: disposeBag)
-        
-        // 상단 X 버튼
-        codeShareView.xButton.rx.tap
-            .asDriver(onErrorDriveWith: .empty())
-            .drive(onNext: { [weak self] in
-                //         let moveIn = MainViewController()
-                self?.navigationController?.popViewController(animated: true)
-            }).disposed(by: disposeBag)
+
+    private func bind() {
         let input = CodeShareViewModel.Input(
-            copyTab: codeShareView.copyRandomCodeButton.rx.tap.asObservable()
+            copyTap: codeShareView.copyRandomCodeButton.rx.tap.asObservable(),
+            mateCodeTap: codeShareView.mateCodeButton.rx.tap.asObservable(),
+            closeTap: codeShareView.xButton.rx.tap.asObservable()
         )
+
         let output = viewModel.transform(input: input)
-        
-        output.showAlert
-            .drive(onNext: { [weak self] alertType in
-                let alert = UIAlertController(
-                    title: alertType.title,
-                    message: alertType.message,
-                    preferredStyle: .alert
-                )
-                alertType.actions.forEach { alert.addAction($0) }
-                self?.present(alert, animated: true)
+
+        output.copiedMessage
+            .drive(onNext: { [weak self] message in
+                self?.showToast(message: message)
             })
             .disposed(by: disposeBag)
-        
-        output.copiedText
-            .bind(to: codeShareView.copyRandomCodeButton.randomCode.rx.text)
+
+        output.navigateToMateCode
+            .drive(onNext: { [weak self] in
+                guard let self = self else { return }
+                let mateVC = MateCodeViewController(uid: self.uid)
+                self.navigationController?.pushViewController(mateVC, animated: true)
+            })
             .disposed(by: disposeBag)
+
+        output.dismiss
+            .drive(onNext: { [weak self] in
+                self?.dismiss(animated: true)
+            })
+            .disposed(by: disposeBag)
+
+        output.inviteCode
+            .drive(codeShareView.copyRandomCodeButton.rx.title(for: .normal))
+            .disposed(by: disposeBag)
+
+        output.showInviteAlert
+            .emit(onNext: { [weak self] nickname in
+                self?.showInviteAlert(from: nickname)
+            })
+            .disposed(by: disposeBag)
+    }
+
+    private func showInviteAlert(from nickname: String) {
+        let alert = UIAlertController(
+            title: "메이트 요청 도착",
+            message: "\(nickname)님이 메이트 요청을 보냈습니다.",
+            preferredStyle: .alert
+        )
+
+        alert.addAction(UIAlertAction(title: "수락", style: .default, handler: { [weak self] _ in
+            guard let self = self else { return }
+            self.viewModel.acceptInvite(fromUid: self.uid)
+                .subscribe(onCompleted: {
+                    self.showToast(message: "메이트가 연결되었습니다 🎉")
+                }, onError: { error in
+                    self.showToast(message: "수락 실패: \(error.localizedDescription)")
+                })
+                .disposed(by: self.disposeBag)
+        }))
+
+        alert.addAction(UIAlertAction(title: "거절", style: .cancel, handler: { [weak self] _ in
+            self?.viewModel.rejectInvite()
+                .subscribe(onCompleted: {
+                    self?.showToast(message: "메이트 요청을 거절했습니다")
+                }, onError: { error in
+                    self?.showToast(message: "거절 실패: \(error.localizedDescription)")
+                })
+                .disposed(by: self!.disposeBag)
+        }))
+
+        present(alert, animated: true)
+    }
+
+    private func showToast(message: String) {
+        let toastLabel = UILabel()
+        toastLabel.text = message
+        toastLabel.textColor = .white
+        toastLabel.backgroundColor = UIColor.black.withAlphaComponent(0.6)
+        toastLabel.font = .systemFont(ofSize: 14)
+        toastLabel.textAlignment = .center
+        toastLabel.alpha = 0
+        toastLabel.layer.cornerRadius = 8
+        toastLabel.clipsToBounds = true
+
+        toastLabel.frame = CGRect(x: 50, y: view.frame.size.height - 100, width: view.frame.size.width - 100, height: 35)
+        view.addSubview(toastLabel)
+
+        UIView.animate(withDuration: 0.5, animations: {
+            toastLabel.alpha = 1.0
+        }) { _ in
+            UIView.animate(withDuration: 0.5, delay: 2.0, options: [], animations: {
+                toastLabel.alpha = 0.0
+            }, completion: { _ in
+                toastLabel.removeFromSuperview()
+            })
+        }
+    }
+
+    deinit {
+        viewModel.stopListening()
     }
 }
+
+// MARK: - 수정전
+//import UIKit
+//import RxSwift
+//import RxCocoa
+//
+//class CodeShareViewController: BaseViewController {
+//    
+//    private let codeShareView = CodeShareView()
+//    private let viewModel: CodeShareViewModel
+//    
+//    private let uid: String // 로그인 사용자 uid
+//    private let nickname: String
+//    
+//    init(uid: String, nickname: String) {
+//        self.uid = uid // 의존성 주입
+//        print("uid : \(uid)")
+//        self.nickname = nickname
+//        viewModel = CodeShareViewModel(uid: uid)
+//        super.init(nibName: nil, bundle: nil)
+//    }
+//    
+//    @MainActor required init?(coder: NSCoder) {
+//        fatalError("init(coder:) has not been implemented")
+//    }
+//    
+//    override func loadView() {
+//        self.view = codeShareView
+//    }
+//    
+//    // 네이게이션 숨기기
+//    override func viewWillAppear(_ animated: Bool) {
+//        super.viewWillAppear(animated)
+//        navigationController?.setNavigationBarHidden(true, animated: false)
+//    }
+//    
+//    override func bindViewModel() {
+//        // 메이트 코드 입력 버튼
+//        codeShareView.mateCodeButton.rx.tap
+//            .asDriver(onErrorDriveWith: .empty())
+//            .drive(onNext: { [weak self] in
+//                guard let self else { return }
+//                let next = MateCodeViewController(uid: self.uid)
+//                self.navigationController?.pushViewController(next, animated: true)
+//            })
+//            .disposed(by: disposeBag)
+//        
+//        // 상단 X 버튼
+//        codeShareView.xButton.rx.tap
+//            .asDriver(onErrorDriveWith: .empty())
+//            .drive(onNext: { [weak self] in
+//                //         let moveIn = MainViewController()
+//                self?.navigationController?.popViewController(animated: true)
+//            }).disposed(by: disposeBag)
+//        let input = CodeShareViewModel.Input(
+//            copyTab: codeShareView.copyRandomCodeButton.rx.tap.asObservable()
+//        )
+//        let output = viewModel.transform(input: input)
+//        
+//        output.showAlert
+//            .drive(onNext: { [weak self] alertType in
+//                let alert = UIAlertController(
+//                    title: alertType.title,
+//                    message: alertType.message,
+//                    preferredStyle: .alert
+//                )
+//                alertType.actions.forEach { alert.addAction($0) }
+//                self?.present(alert, animated: true)
+//            })
+//            .disposed(by: disposeBag)
+//        
+//        output.copiedText
+//            .bind(to: codeShareView.copyRandomCodeButton.randomCode.rx.text)
+//            .disposed(by: disposeBag)
+//    }
+//}
--- FitMate/FitMate/Scene/CodeShare/ViewModel/CodeShareViewModel.swift
+++ FitMate/FitMate/Scene/CodeShare/ViewModel/CodeShareViewModel.swift
@@ -4,66 +4,202 @@
 //  Created by 강성훈 on 6/5/25.
 //
 
-import UIKit
+import Foundation
 import RxSwift
 import RxCocoa
+import FirebaseFirestore
 
-class CodeShareViewModel {
+final class CodeShareViewModel: ViewModelType {
     
-    private let copyRelay = BehaviorRelay<String>(value: "")
-    private let copyTapRelay = PublishRelay<Void>()
-    private let alertRelay = PublishRelay<SystemAlertType>()
+    // MARK: - Input / Output
+    struct Input {
+        let copyTap: Observable<Void>
+        let mateCodeTap: Observable<Void>
+        let closeTap: Observable<Void>
+    }
+
+    struct Output {
+        let copiedMessage: Driver<String>
+        let navigateToMateCode: Driver<Void>
+        let dismiss: Driver<Void>
+        let showInviteAlert: Signal<String>
+        let inviteCode: Driver<String>
+    }
+
+    // MARK: - Properties
+    private let uid: String
+    private let firestoreService = FirestoreService.shared
+    private let db = Firestore.firestore()
+    private let disposeBag = DisposeBag()
     
-    let uid: String
+    private let inviteAlertRelay = PublishRelay<String>()
+    private let inviteCodeRelay = BehaviorRelay<String>(value: "")
+    private var nickname: String = ""
+    private var listener: ListenerRegistration?
     
     init(uid: String) {
         self.uid = uid
+        fetchMyUserInfo()
+        startListeningInviteStatus()
     }
     
-    var disposeBag = DisposeBag()
-    
-    struct Input {
-        let copyTab: Observable<Void>
+    func setNickname(_ name: String) {
+        self.nickname = name
     }
     
-    struct Output {
-        let copiedText: Observable<String>
-        let showAlert: Driver<SystemAlertType>
-    }
-    
-    /// 외부에서 전달받은 코드를 내부 relay로 전달
-    func setCode(_ code: String) {
-        copyRelay.accept(code)
-    }
-    
+    // MARK: - Transform
     func transform(input: Input) -> Output {
-        FirestoreService.shared
-            .fetchDocument(collectionName: "codes", documentName: "mateCode")
-        // 가져온 Document에서 "value" 필드를 꺼내 String으로 변환
-            .map { $0["value"] as? String ?? "" }
-            .subscribe(onSuccess: { [weak self] code in
-                // 성공적으로 값을 받아오면 setCode 메서드를 통해 relay에 저장
-                self?.setCode(code)
-            }, onFailure: { error in
-                print("Firestore fetch error: \(error.localizedDescription)")
-            })
-            .disposed(by: disposeBag)
-        
-        // 복사 버튼
-        input.copyTab
-            .withLatestFrom(copyRelay)
-            .do(onNext: { code in
-                UIPasteboard.general.string = code
-            })
-            .map { _ in
-                SystemAlertType.copied
+        // 1. 복사 버튼 탭
+        let copiedMessage = input.copyTap
+            .map { [weak self] in
+                guard let self else { return "복사 실패"}
+                UIPasteboard.general.string = self.inviteCodeRelay.value
+                return "초대 코드가 복사되었습니다"
             }
-            .bind(to: alertRelay)
-            .disposed(by: disposeBag)
-        
+            .asDriver(onErrorJustReturn: "복사 실패")
+
+        // 2. 화면 전환
+        let navigateToMateCode = input.mateCodeTap
+            .asDriver(onErrorJustReturn: ())
+
+        // 3. 닫기
+        let dismiss = input.closeTap
+            .asDriver(onErrorJustReturn: ())
+
         return Output(
-            copiedText: copyRelay.asObservable(),
-            showAlert: alertRelay.asDriver(onErrorDriveWith: .empty())
+            copiedMessage: copiedMessage,
+            navigateToMateCode: navigateToMateCode,
+            dismiss: dismiss,
+            showInviteAlert: inviteAlertRelay.asSignal(),
+            inviteCode: inviteCodeRelay.asDriver()
         )
     }
+    
+    private func fetchMyUserInfo() {
+        firestoreService.fetchDocument(collectionName: "users", documentName: uid)
+            .subscribe(onSuccess: { [weak self] data in
+                self?.nickname = data["nickname"] as? String ?? "알 수 없음"
+                let code = data["inviteCode"] as? String ?? "------"
+                self?.inviteCodeRelay.accept(code)
+            }, onFailure: { error in
+                print("유저 정보 가져오기 실패: \(error.localizedDescription)")
+            })
+            .disposed(by: disposeBag)
+    }
+
+    // MARK: - Listen for Invite
+    private func startListeningInviteStatus() {
+        let ref = db.collection("users").document(uid)
+        
+        listener = ref.addSnapshotListener { [weak self] snapshot, error in
+            guard let self = self,
+                  let data = snapshot?.data(),
+                  let status = data["inviteStatus"] as? String,
+                  status == "invited",
+                  let fromUid = data["fromUid"] as? String else {
+                return
+            }
+            
+            self.firestoreService.fetchDocument(collectionName: "users", documentName: fromUid)
+                .subscribe(onSuccess: { doc in
+                    let nickname = doc["nickname"] as? String ?? "상대방"
+                    self.inviteAlertRelay.accept(nickname)
+                })
+                .disposed(by: self.disposeBag)
+        }
+    }
+
+    // MARK: - Actions
+    func acceptInvite(fromUid: String) -> Completable {
+        let updateMyDoc = firestoreService.updateDocument(collectionName: "users", documentName: uid, fields: [
+            "inviteStatus": "accepted",
+            "mate": ["uid": fromUid, "nickname": "상대방"],
+            "updatedAt": FieldValue.serverTimestamp()
+        ])
+        
+        let updateOtherDoc = firestoreService.updateDocument(collectionName: "users", documentName: fromUid, fields: [
+            "inviteStatus": "accepted",
+            "mate": ["uid": uid, "nickname": nickname],
+            "updatedAt": FieldValue.serverTimestamp()
+        ])
+        
+        return Completable.zip(updateMyDoc.asCompletable(), updateOtherDoc.asCompletable())
+    }
+    
+    func rejectInvite() -> Completable {
+        return firestoreService.updateDocument(collectionName: "users", documentName: uid, fields: [
+            "fromUid": FieldValue.delete(),
+            "inviteStatus": "waiting",
+            "updatedAt": FieldValue.serverTimestamp()
+        ]).asCompletable()
+    }
+    
+    func stopListening() {
+        listener?.remove()
+    }
 }
+
+// MARK: - 수정 전
+//import UIKit
+//import RxSwift
+//import RxCocoa
+//
+//class CodeShareViewModel {
+//    
+//    private let copyRelay = BehaviorRelay<String>(value: "")
+//    private let copyTapRelay = PublishRelay<Void>()
+//    private let alertRelay = PublishRelay<SystemAlertType>()
+//    
+//    let uid: String
+//    
+//    init(uid: String) {
+//        self.uid = uid
+//    }
+//    
+//    var disposeBag = DisposeBag()
+//    
+//    struct Input {
+//        let copyTab: Observable<Void>
+//    }
+//    
+//    struct Output {
+//        let copiedText: Observable<String>
+//        let showAlert: Driver<SystemAlertType>
+//    }
+//    
+//    /// 외부에서 전달받은 코드를 내부 relay로 전달
+//    func setCode(_ code: String) {
+//        copyRelay.accept(code)
+//    }
+//    
+//    func transform(input: Input) -> Output {
+//        FirestoreService.shared
+//            .fetchDocument(collectionName: "codes", documentName: "mateCode")
+//        // 가져온 Document에서 "value" 필드를 꺼내 String으로 변환
+//            .map { $0["value"] as? String ?? "" }
+//            .subscribe(onSuccess: { [weak self] code in
+//                // 성공적으로 값을 받아오면 setCode 메서드를 통해 relay에 저장
+//                self?.setCode(code)
+//            }, onFailure: { error in
+//                print("Firestore fetch error: \(error.localizedDescription)")
+//            })
+//            .disposed(by: disposeBag)
+//        
+//        // 복사 버튼
+//        input.copyTab
+//            .withLatestFrom(copyRelay)
+//            .do(onNext: { code in
+//                UIPasteboard.general.string = code
+//            })
+//            .map { _ in
+//                SystemAlertType.copied
+//            }
+//            .bind(to: alertRelay)
+//            .disposed(by: disposeBag)
+//        
+//        return Output(
+//            copiedText: copyRelay.asObservable(),
+//            showAlert: alertRelay.asDriver(onErrorDriveWith: .empty())
+//        )
+//    }
+//}
