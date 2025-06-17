08e2e747c34c5ebea2cc9d2d3e6b756142b662a8
Charlie_Kang <168897977+tututu08@users.noreply.github.com>
2025-06-18 01:08:56 +0900

    [Design] #56 플랭크 화면 UI구현 + 기능 로직 구현 + Alert수정 (#87)
    * [Merge] #56 dev into #56
    
    * [Refactor] #56 Alert 타입 변경 및 타이머 기능추가
    
    * [Feat] #56 Plank 협력 모드 기능 구현, 테스트용 피니쉬 화면 추가.
    
    * [Refactor] #56 Alert 설정(Stop,Pause) 및  위치 변경& 구조 변경
    
    * [Chore] #56 이미지,폰트 변경 및 패키지 추가
    
    * [Feat] #56 알럿 연결 로직 구현.
    
    * [Chore] #56 플랭크 조건문 수정
    
    * [Chore] #56 컬러 변경
    
    * [Chore] #56 가독성 위한 변수명 변경 phase -> status
    
    * [Chore] #56 이미지 파일명 변경
    
    * [Refactor] #56 Alert 호출, 숨기기 메서드명 변경

--- FitMate/FitMate/Common/Components/BattleSportsView.swift
+++ FitMate/FitMate/Common/Components/BattleSportsView.swift
@@ -39,7 +39,6 @@ class BattleSportsView: BaseView {
         label.textColor = .white
         label.font = .boldSystemFont(ofSize: 25)
         label.setContentHuggingPriority(.required, for: .horizontal)
-
         return label
     }()
     
@@ -103,13 +102,13 @@ class BattleSportsView: BaseView {
     private let myProgressBackgroundView: UIView = {
         let view = UIView()
         view.layer.borderWidth = 2
-        view.layer.borderColor = UIColor.systemPurple.cgColor
+        view.layer.borderColor = UIColor.primary500.cgColor
         return view
     }()
     
     private let myProgressForegroundView: UIView = {
         let view = UIView()
-        view.backgroundColor = .systemPurple
+        view.backgroundColor = .primary500
         return view
     }()
     
@@ -146,7 +145,7 @@ class BattleSportsView: BaseView {
     private let myCharacterImage: UIImageView = {
         let imageView = UIImageView()
         imageView.image = UIImage(named: "KaepyR")
-        imageView.contentMode = .scaleAspectFill
+        imageView.contentMode = .scaleAspectFit
         return imageView
     }()
     
@@ -154,19 +153,16 @@ class BattleSportsView: BaseView {
         let imageView = UIImageView()
         imageView.image = UIImage(named: "MeoranoL")
         imageView.transform = CGAffineTransform(scaleX: -1, y: 1)
-        imageView.contentMode = .scaleAspectFill
+        imageView.contentMode = .scaleAspectFit
         return imageView
-    }()
-    
-    private let pauseButton: UIButton = {
-        let button = UIButton()
-        button.setImage(UIImage(named: "pause"), for: .normal)
-        return button
     }()
     
     private let stopButton: UIButton = {
         let button = UIButton()
-        button.setImage(UIImage(named: "stop"), for: .normal)
+        button.setTitle("그만하기", for: .normal)
+        button.titleLabel?.font = UIFont(name: "Pretendard-Regular", size: 20)
+        button.setTitleColor(.white, for: .normal)
+        button.setBackgroundImage(UIImage(named: "350button"), for: .normal)
         return button
     }()
     
@@ -182,7 +178,6 @@ class BattleSportsView: BaseView {
           goalImage,
           totalVerticalStack,
           battleImage,
-          pauseButton,
           stopButton
         ].forEach{self.addSubview($0)}
         
@@ -190,7 +185,7 @@ class BattleSportsView: BaseView {
     
     override func setLayoutUI() {
         modeLabel.snp.makeConstraints {
-            $0.top.equalTo(safeAreaLayoutGuide).offset(0)
+            $0.top.equalToSuperview().offset(60)
             $0.centerX.equalToSuperview()
         }
         
@@ -229,36 +224,31 @@ class BattleSportsView: BaseView {
         }
         
         battleImage.snp.makeConstraints {
-            $0.bottom.equalTo(stopButton.snp.top).inset(-40)
+            $0.bottom.equalTo(stopButton.snp.top).inset(-50)
             $0.centerX.equalToSuperview()
             $0.width.equalTo(350)
-            $0.height.equalTo(340)
+            $0.height.equalTo(310)
         }
         
         myCharacterImage.snp.makeConstraints{
-            $0.leading.equalTo(battleImage.snp.leading).inset(10)
-            $0.bottom.equalTo(battleImage.snp.bottom).inset(41)
+            $0.leading.equalTo(battleImage.snp.leading).inset(13)
+            $0.bottom.equalTo(battleImage.snp.bottom).inset(10)
             $0.height.equalTo(150)
             $0.width.equalTo(120)
         }
         
         mateCharacterImage.snp.makeConstraints{
-            $0.trailing.equalTo(battleImage.snp.trailing).inset(10)
-            $0.bottom.equalTo(battleImage.snp.bottom).inset(160)
+            $0.trailing.equalTo(battleImage.snp.trailing).inset(13)
+            $0.bottom.equalTo(battleImage.snp.bottom).inset(150)
             $0.height.equalTo(150)
             $0.width.equalTo(120)
         }
-        pauseButton.snp.makeConstraints{
-            $0.bottom.equalToSuperview().inset(30)
-            $0.leading.equalToSuperview().offset(20)
-            $0.width.height.equalTo(60)
-        }
         
         stopButton.snp.makeConstraints{
-            $0.bottom.equalToSuperview().inset(30)
-            $0.leading.equalTo(pauseButton.snp.trailing).offset(12)
+            $0.bottom.equalToSuperview().inset(60)
+            $0.centerX.equalToSuperview()
             $0.height.equalTo(60)
-            $0.width.equalTo(260)
+            $0.width.equalTo(350)
         }
     }
     // 내 기록 라벨 갱신
