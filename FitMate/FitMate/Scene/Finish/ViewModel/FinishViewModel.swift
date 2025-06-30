aefaa9dbc3c40d2b9a7f431b5a976e1e29c19f8a
NH <mnh4140@gmail.com>
2025-06-30 23:37:17 +0900

    [Chore] #293 필요없는 주석 삭제 및 코드 열 맞추기

--- FitMate/FitMate/Scene/Finish/ViewModel/FinishViewModel.swift
+++ FitMate/FitMate/Scene/Finish/ViewModel/FinishViewModel.swift
@@ -128,10 +128,8 @@ extension FinishViewModel {
                   let players = data["players"] as? [String: Any],
                   let myData = players[uid] as? [String: Any],
                   let mateData = players[mateUid] as? [String: Any],
-//                  let myProgress = myData["progress"] as? Int,
-//                  let mateProgress = mateData["progress"] as? Int else {
-                    let myProgress = myData["progress"] as? Double,
-                    let mateProgress = mateData["progress"] as? Double else {
+                  let myProgress = myData["progress"] as? Double,
+                  let mateProgress = mateData["progress"] as? Double else {
                 return .error(NSError(domain: "", code: -2, userInfo: [NSLocalizedDescriptionKey: "필드 누락 또는 변환 실패"]))
             }
 
@@ -146,14 +144,6 @@ extension FinishViewModel {
                 }
             }()
 
-//            let record = ExerciseRecord(
-//                type: exerciseType,
-//                date: self.formatDate(timestamp.dateValue()),
-//                result: result,
-//                detail1: "\(goalValue)",
-//                detail2: "\(myProgress)",
-//                detail3: "\(mateProgress)"
-//            )
             let detail2: String
             let detail3: String
 
