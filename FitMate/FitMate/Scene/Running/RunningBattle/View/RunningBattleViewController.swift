106e1b9bdb9b1b65c855c79d8d3b042fab44f129
NH <mnh4140@gmail.com>
2025-06-30 23:40:32 +0900

    [Fix] #293 navigateToFinish 함수 추가

--- FitMate/FitMate/Scene/Running/RunningBattle/View/RunningBattleViewController.swift
+++ FitMate/FitMate/Scene/Running/RunningBattle/View/RunningBattleViewController.swift
@@ -3,6 +3,7 @@ import UIKit
 import Foundation
 import RxCocoa
 import CoreLocation
+import FirebaseFirestore
 
 class RunningBattleViewController: BaseViewController {
     
@@ -179,13 +180,47 @@ class RunningBattleViewController: BaseViewController {
         present(vc, animated: true)
     }
     
+    private func navigateToFinish() {
+        Firestore.firestore().collection("matches").document(matchCode)
+            .getDocument { [weak self] snapshot, error in
+                guard let self = self else { return }
+                guard let data = snapshot?.data(),
+                      let players = data["players"] as? [String: Any],
+                      let myData = players[self.myUid] as? [String: Any],
+                      let isWinner = myData["isWinner"] as? Bool else {
+                    print("승자 정보 불러오기 실패")
+                    return
+                }
+                
+                let finishVM = FinishViewModel(
+                    mode: .battle,
+                    sport: exerciseType,
+                    goal: goalDistance,
+                    goalUnit: "Km",
+                    myDistance: self.viewModel.myDistanceRelay.value,
+                    character: self.myCharacter,
+                    success: isWinner  // Firestore에서 가져온 최종 결과
+                )
+                
+                let vc = FinishViewController(
+                    uid: self.myUid,
+                    mateUid: self.mateUid,
+                    matchCode: self.matchCode,
+                    viewModel: finishVM
+                )
+                vc.modalPresentationStyle = .fullScreen
+                self.present(vc, animated: true)
+            }
+    }
+    
     func receiveMateQuit() {
         viewModel.stopLocationUpdates()
         rootView.showQuitAlert(
             type: .mateQuit,
             onBack: { [weak self] in
                 self?.viewModel.finish(success: true)
-                self?.navigateToFinish(success: true, myDistance: self?.viewModel.myDistanceRelay.value ?? 0.0)
+                //self?.navigateToFinish(success: true, myDistance: self?.viewModel.myDistanceRelay.value ?? 0.0)
+                self?.navigateToFinish()
             }
         )
     }
