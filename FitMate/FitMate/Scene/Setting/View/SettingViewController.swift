543550f26748e2edb153370abce726827af71d21
Sophie4han <hanshirley775@gamil.com>
2025-07-03 09:32:29 +0900

    [Fix] #312 회원 탈퇴 시 아바타 초기화

--- FitMate/FitMate/Scene/Setting/View/SettingViewController.swift
+++ FitMate/FitMate/Scene/Setting/View/SettingViewController.swift
@@ -152,6 +152,19 @@ final class SettingViewController: UIViewController {
                 self?.settingView.isHidden = false
             }
             .disposed(by: disposeBag)
+        
+        popup.confirmButton.rx.tap
+            .flatMapLatest { [weak self] _ -> Observable<Void> in
+                guard let self else { return .empty() }
+                return self.performWithdrawProcess()
+            }
+            .subscribe(onNext: { [weak self] in
+                AvatarManager.shared.reset()
+                self?.navigateToLogin()
+            }, onError: { error in
+                print("회원 탈퇴 실패: \(error.localizedDescription)")
+            })
+            .disposed(by: disposeBag)
     }
 
     // 회원 탈퇴 전체 처리 로직 (구글 재인증 포함)
@@ -368,4 +381,3 @@ final class SettingViewController: UIViewController {
     }
 
 }
- 
