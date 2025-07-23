370504b9173ee0bf74551d8f7a35a44d7f563593
NH <mnh4140@gmail.com>
2025-07-23 16:28:48 +0900

    [Fix] #364 @MainActor 사용 부분 제거

--- FitMate/FitMate/Scene/Shop/View/ShopView.swift
+++ FitMate/FitMate/Scene/Shop/View/ShopView.swift
@@ -96,7 +96,7 @@ class ShopView: BaseView {
         }
     }
     
-    @MainActor required init?(coder: NSCoder) {
+    required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
--- FitMate/FitMate/Scene/Shop/View/ShopViewController.swift
+++ FitMate/FitMate/Scene/Shop/View/ShopViewController.swift
@@ -27,7 +27,7 @@ class ShopViewController: BaseViewController, UICollectionViewDelegateFlowLayout
         super.init(nibName: nil, bundle: nil)
     }
     
-    @MainActor required init?(coder: NSCoder) {
+    required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
