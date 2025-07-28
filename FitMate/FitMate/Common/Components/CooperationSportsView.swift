21128d0af8c48ab05f1f88b548ce2581b9d08d00
NH <mnh4140@gmail.com>
2025-07-28 17:12:38 +0900

    [Fix] #393 CooperationSportsView 폰트 통일

--- FitMate/FitMate/Common/Components/CooperationSportsView.swift
+++ FitMate/FitMate/Common/Components/CooperationSportsView.swift
@@ -22,7 +22,7 @@ class CooperationSportsView: BaseView {
         let label = UILabel()
         label.text = "종목 목표치"
         label.textColor = .black
-        label.font = .boldSystemFont(ofSize: 20)
+        label.font = UIFont(name: "Pretendard-Medium", size: 20)
         return label
     }()
     
@@ -30,7 +30,7 @@ class CooperationSportsView: BaseView {
         let label = UILabel()
         label.text = "나"
         label.textColor = .white
-        label.font = .boldSystemFont(ofSize: 24)
+        label.font = UIFont(name: "Pretendard-SemiBold", size: 24)
         return label
     }()
     
@@ -38,7 +38,7 @@ class CooperationSportsView: BaseView {
         let label = UILabel()
         label.text = "메이트"
         label.textColor = .white
-        label.font = .boldSystemFont(ofSize: 24)
+        label.font = UIFont(name: "Pretendard-SemiBold", size: 24)
         return label
     }()
     
@@ -46,7 +46,7 @@ class CooperationSportsView: BaseView {
         let label = UILabel()
         label.text = "나의기록"
         label.textColor = .gray
-        label.font = .boldSystemFont(ofSize: 15)
+        label.font = UIFont(name: "Pretendard-Medium", size: 14)
         return label
     }()
     
@@ -54,7 +54,7 @@ class CooperationSportsView: BaseView {
         let label = UILabel()
         label.text = "메이트기록"
         label.textColor = .gray
-        label.font = .boldSystemFont(ofSize: 15)
+        label.font = UIFont(name: "Pretendard-Medium", size: 14)
         return label
     }()
     
