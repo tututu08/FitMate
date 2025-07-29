97166c8d69e0d697ffc3d3f4cf779fbe6cb7afd0
NH <mnh4140@gmail.com>
2025-07-29 15:28:27 +0900

    [Fix] #393 MateEndPopupView 폰트 색상 통일

--- FitMate/FitMate/Scene/Setting/View/MateEndPopupView.swift
+++ FitMate/FitMate/Scene/Setting/View/MateEndPopupView.swift
@@ -14,7 +14,7 @@ final class MateEndPopupView: UIView {
     //팝업 메인 ㄴ컨테이너
     let containerView: UIView = {
         let view = UIView()
-        view.backgroundColor = .white
+        view.backgroundColor = .background0
         view.layer.cornerRadius = 8
         return view
     }()
@@ -54,7 +54,7 @@ final class MateEndPopupView: UIView {
     let confirmButton: UIButton = {
         let button = UIButton()
         button.setTitle("종료", for: .normal)
-        button.setTitleColor(.white, for: .normal)
+        button.setTitleColor(.background0, for: .normal)
         button.backgroundColor = UIColor(named: "Primary500")
         button.layer.cornerRadius = 4
         return button
